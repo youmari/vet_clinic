ced94cbee4687db50509f31b18b8a90b3c830581
youmari <youmari300@gmail.com>
2022-02-15 15:57:30 +0100

    Add records to the Table animals

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals(
-    id INT NOT NULL, 
-    name VARCHAR(100) NOT NULL, 
-    date_of_birth DATE, 
-    escape_attemps INT, 
-    neutered BOOLEAN, 
-    weight_kg DECIMAL
+CREATE TABLE animals (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(100),
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
 );
