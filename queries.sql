96a62d8d765ad6acc9ec263eabefa6399d73cc82
youmari <youmari300@gmail.com>
2022-02-16 00:03:56 +0100

    Add queries

--- queries.sql
+++ queries.sql
@@ -15,3 +15,15 @@ SELECT * FROM animals WHERE neutered = 'true';
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= '10.4' AND weight_kg <= '17.3';
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, COUNT(escape_attempts) FROM animals GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
