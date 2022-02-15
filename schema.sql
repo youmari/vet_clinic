240fd6d6e76ecd11a8fbe2b2a7f5a5cf94eec060
youmari <youmari300@gmail.com>
2022-02-15 15:25:19 +0100

    Create animals table with their columns

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+    id INT NOT NULL, 
+    name VARCHAR(100) NOT NULL, 
+    date_of_birth DATE, 
+    escape_attemps INT, 
+    neutered BOOLEAN, 
+    weight_kg DECIMAL
 );
