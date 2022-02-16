9e2179a5e1746c3da268489c7b6ee584fba26069
youmari <youmari300@gmail.com>
2022-02-16 08:52:52 +0100

    Add records to the tables

--- data.sql
+++ data.sql
@@ -1,14 +1,46 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (1,'Agumon',  '2020/02/03', '0', 'true', '10.23'),
-       (2,'Gabumon', '2018/11/15', '2', 'true', '8'),
-       (3,'Pikachu', '2021/01/07', '1', 'false', '15.04'),
-       (4,'Devimon', '2017/05/12', '5', 'true', '11');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Agumon',  '2020/02/03', '0', 'true', '10.23'),
+       ('Gabumon', '2018/11/15', '2', 'true', '8'),
+       ('Pikachu', '2021/01/07', '1', 'false', '15.04'),
+       ('Devimon', '2017/05/12', '5', 'true', '11');
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES  (5, 'Charmander', '2020-02-08', 0, false, -11),
-        (6, 'Plantmon', '2022-11-15', 2, true, -5.7),
-        (7,'Angemon', '2005-06-12', 1, true, -45),
-        (8,'Boarmon', '2005-06-07', 7, true, 20.4),
-        (9,'Blossom', '1998-10-13', 3, true, 17);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES  ('Charmander', '2020-02-08', 0, false, -11),
+        ('Plantmon', '2022-11-15', 2, true, -5.7),
+        ('Angemon', '2005-06-12', 1, true, -45),
+        ('Boarmon', '2005-06-07', 7, true, 20.4),
+        ('Blossom', '1998-10-13', 3, true, 17);
+
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+       ('Jennifer Orwell', 19),
+       ('Bob', 45),
+       ('Melody Pond', 77),
+       ('Dean Winchester', 14),
+       ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES ('Pokemon'), ('Digimon');
+
+UPDATE animals 
+SET species_id = 1 WHERE name LIKE '%mon';
+
+UPDATE animals 
+SET species_id = 2 WHERE species_id IS NULL;
+
+UPDATE animals
+SET owner_id = 1 WHERE name LIKE 'Agumon';
+
+UPDATE animals
+SET owner_id = 2 WHERE name LIKE 'Gabumon' OR name LIKE 'Pikachu'; 
+
+UPDATE animals
+SET owner_id = 3 WHERE name LIKE 'Devimon' OR name like 'Plantmon'; 
+
+UPDATE animals
+SET owner_id = 4 WHERE name LIKE 'Charmander' OR name like 'Squirtle' OR name LIKE 'Blossom';
+
+UPDATE animals
+SET owner_id = 5 WHERE name LIKE 'Angemon' OR name like 'Boarmon'; 
